1272379146bd961d9eb9bdf6e5feb6055facc946
Kwak-DH <eogns6595@gmail.com>
2026-09-18 11:05:28 +0900

    React + TypeScript 프로젝트 초기 세팅 및 Hello World

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen p-[50px]">
+      <div className="flex min-h-[calc(100vh-100px)] items-center justify-center bg-yellow-300">
+        <h1 className="text-4xl font-bold">Hello World</h1>
+      </div>
+    </div>
+  );
+}
